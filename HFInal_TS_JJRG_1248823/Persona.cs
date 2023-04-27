a1d72e36f0da33ea6e5e1eeb45ff1690867ff051
JulioRob23 <123261341+JulioRob23@users.noreply.github.com>
2023-04-27 13:53:13 -0600

    Ejercicio Persona

---
+++ HFInal_TS_JJRG_1248823/Persona.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HFInal_TS_JJRG_1248823
+{
+    class Persona
+    {
+        private string nombre, apellido;
+        private int edad;
+        private double alturacm;
+
+        public string NombreCompleto()
+        {
+            string nombrec = "" + nombre + " " + apellido;
+            return nombrec;
+        }
+        public double Alturaenm()
+        {
+            double alturam = alturacm / 100;
+            return alturam;
+        }
+        public void Imprimir()
+        {
+            Console.WriteLine("Persona: " + NombreCompleto());
+            Console.WriteLine("Edad: " + edad);
+            Console.WriteLine("Altura: " + Alturaenm());
+         
+        }
+        public Persona(string elnombre, string elapellido, int laedad, double laalturacm)
+        {
+            nombre = elnombre;
+            apellido = elapellido;
+            edad = laedad;
+            alturacm = laalturacm;
+        }
+
+    }
+}
---
+++ HFInal_TS_JJRG_1248823/Program.cs
@@ -0,0 +1,23 @@
+﻿using HFInal_TS_JJRG_1248823;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Ejercicio 1");
+        Console.WriteLine("Ejercicio Personas:");
+        Console.WriteLine("Ingrese el nombre");
+        string nom = Console.ReadLine();
+        Console.WriteLine("Ingrese el apellido");
+        string ape = Console.ReadLine();
+        Console.WriteLine("Ingrese la edad");
+        int ed = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Ingrese la altura en cm");
+        double cm = Convert.ToDouble(Console.ReadLine());
+
+        Persona persona1 = new Persona(nom, ape,ed,cm);
+        persona1.Imprimir();
+        Console.ReadKey();
+    }
+}
